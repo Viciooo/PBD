221c9b0e3bde8a2b9ab9ce4376ffb2bf1aed57a8
Viciooo <72527323+Viciooo@users.noreply.github.com>
2021-10-17 18:44:40 +0200

    Create wprowadzenie.sql

---
+++ wprowadzenie.sql
@@ -0,0 +1,65 @@
+SELECT keyword
+
+1. Wybierz nazwy i adresy wszystkich klientów:
+SELECT CompanyName,Address,City,Region,PostalCode,Country FROM Customers
+
+2. Wybierz nazwiska i numery telefonów pracowników:
+SELECT LastName, HomePhone FROM Employees
+
+3. Wybierz nazwy i ceny produktów:
+SELECT ProductName, UnitPrice FROM Products
+
+4. Pokaż wszystkie kategorie produktów (nazwy i opisy):
+SELECT CategoryName, Description FROM Categories
+
+5. Pokaż nazwy i adresy stron www dostawców:
+SELECT CompanyName, HomePage from Suppliers
+	
+WHERE keyword	
+
+1. Wybierz nazwy i adresy wszystkich klientów mających siedziby w Londynie:
+SELECT CompanyName, Country, Region, City, PostalCode, Address FROM Customers WHERE City = 'London'
+
+2. Wybierz nazwy i adresy wszystkich klientów mających siedziby we Francji lub w Hiszpanii:
+SELECT CompanyName, Country, Region, City, PostalCode, Address FROM Customers WHERE Country = 'France' or Country = 'Spain'
+
+3. Wybierz nazwy i ceny produktów o cenie jednostkowej pomiędzy 20.00 a 30.00:
+SELECT ProductName, UnitPrice FROM Products WHERE UnitPrice >= 20.00 AND UnitPrice <= 30.00
+
+4. Wybierz nazwy i ceny produktów z kategorii ‘meat’:
+SELECT ProductName, UnitPrice FROM Products WHERE CategoryID = (SELECT CategoryID FROM Categories WHERE CategoryName = 'Meat/Poultry')
+
+5. Wybierz nazwy produktów oraz inf. o stanie magazynu dla produktów dostarczanych przez firmę ‘Tokyo Traders’:
+SELECT SupplierID FROM Suppliers WHERE CompanyName = 'Tokyo Traders' SELECT ProductName, UnitsInStock FROM Products WHERE SupplierID = 4
+
+6. Wybierz nazwy produktów których nie ma w magazynie:
+SELECT ProductName FROM Products WHERE UnitsInStock = 0
+	
+LIKE keyword	
+
+1. Szukamy informacji o produktach sprzedawanych w butelkach (‘bottle’):
+SELECT ProductName FROM Products WHERE QuantityPerUnit LIKE '%bottle%'
+
+2. Wyszukaj informacje o stanowisku pracowników, których nazwiska zaczynają się na literę z zakresu od B do L:
+SELECT Title FROM Employees WHERE LastName LIKE '[B-L]%'
+3. Wyszukaj informacje o stanowisku pracowników, których nazwiska zaczynają się na literę B lub L:
+SELECT Title,LastName FROM Employees WHERE LastName LIKE '[BL]%'
+4. Znajdź nazwy kategorii, które w opisie zawierają przecinek:
+SELECT CategoryName FROM Categories WHERE Categories.Description LIKE '%,%'
+
+5. Znajdź klientów, którzy w swojej nazwie mają w którymś miejscu słowo ‘Store’:
+SELECT CompanyName FROM Customers WHERE CompanyName LIKE '%Store%'
+	
+Zakres wartości - ćwiczenie	
+
+1. Szukamy informacji o produktach o cenach mniejszych niż 10 lub większych niż 20:
+SELECT ProductName,UnitPrice FROM Products WHERE UnitPrice < 10 OR UnitPrice > 20
+
+2. Wybierz nazwy i ceny produktów o cenie jednostkowej pomiędzy 20.00 a 30.00:
+SELECT ProductName,UnitPrice FROM Products WHERE UnitPrice BETWEEN 20 AND 30
+	
+Warunki logiczne - ćwiczenie
+
+1. Wybierz nazwy i kraje wszystkich klientów mających siedziby w Japonii (Japan) lub we Włoszech (Italy):
+SELECT CompanyName,Country FROM Customers WHERE Country = 'Japan' OR Country='Italy'
+	
