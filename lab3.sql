f6f1e698c9930e7e94b60ad591413c19d43f4536
Viciooo <72527323+Viciooo@users.noreply.github.com>
2021-10-25 13:45:04 +0200

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -30,3 +30,51 @@ WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products)
 SELECT SUM(UnitPrice*Quantity*(1-Discount))
 FROM [order details]
 WHERE OrderId = 10250
+
+-- #######################################################################################################################################################################
+
+USE Northwind
+SELECT *
+FROM orderhist
+
+USE Northwind
+SELECT ProductID,SUM(Quantity),AVG(Quantity)
+FROM [Order Details]
+WHERE productid < 2
+GROUP BY ProductID
+
+-- 1. Podaj maksymalną cenę zamawianego produktu dla każdego zamówienia
+select OrderID, MAX(UnitPrice) as "MAX"
+from [Order Details]
+group by OrderID
+
+-- 2. Posortuj zamówienia wg maksymalnej ceny produktu
+select OrderID, MAX(UnitPrice) as 'Max'
+from [Order Details]
+group by OrderID
+order by 2 desc
+
+-- 3. Podaj maksymalną i minimalną cenę zamawianego produktu dla każdego zamówienia
+select OrderID, MAX(UnitPrice) as "MAX", MIN(UnitPrice) as "MIN"
+from [Order Details]
+group by OrderID
+
+-- 4. Podaj liczbę zamówień dostarczanych przez poszczególnych spedytorów
+-- (przewoźników).
+SELECT COUNT(*) as Counter, ShipVia, (SELECT CompanyName FROM Shippers WHERE ShipperID = ShipVia) AS ShipperName
+FROM Orders
+GROUP BY ShipVia
+
+-- 5. Który z spedytorów był najaktywniejszy w 1997 roku.
+SELECT TOP 1 COUNT(*) as Counter, ShipVia, (SELECT CompanyName FROM Shippers WHERE ShipperID = ShipVia) AS ShipperName
+FROM Orders
+WHERE YEAR(ShippedDate) = 1997
+GROUP BY ShipVia
+ORDER BY Counter DESC
+
+-- 6 Który z spedytorów zarobił najwięcej w 1997 roku.
+SELECT TOP 1 SUM(Freight) as value, ShipVia
+FROM Orders
+WHERE YEAR(ShippedDate) = 1997
+GROUP BY ShipVia
+ORDER BY value DESC
